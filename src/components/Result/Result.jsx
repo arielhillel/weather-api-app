05837d63991dec1b4c990d514621bb249491bdcc
Ariel Hillel <arielhillel77@gmail.com>
2022-03-23 16:50:05 +0200

    final commit

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -11,13 +11,13 @@ export const Result = () => {
   if (weaterState === 0) {
     return (
       <div className="resultCard">
-        <Card className="card-xxx">
+        <Card className="card-info">
           <Card.Img
             variant="top"
             src="https://www.timeanddate.com/scripts/weather_og.php?h1=Weather&h2=Local%20Weather%20Around%20the%20World"
           />
           <Card.Body>
-            <Card.Title>Enter a city</Card.Title>
+            <Card.Title>There is nothing to show !</Card.Title>
             <img
               src={weaterState?.lastSearthResult?.current?.condition?.icon}
             />
@@ -28,7 +28,7 @@ export const Result = () => {
   }
   return (
     <div className="resultCard">
-      <Card className="card-xxx">
+      <Card className="card-info">
         <Card.Img
           variant="top"
           src="https://www.timeanddate.com/scripts/weather_og.php?h1=Weather&h2=Local%20Weather%20Around%20the%20World"
